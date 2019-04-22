4bde2901fda877f14b3a3bdfc288cb4fb229d7a7
thaycacac <thaycacac@gmail.com>
2019-04-22 14:46:36 +0700

    add: video

--- components/blog/index.vue
+++ components/blog/index.vue
@@ -4,70 +4,112 @@
       Chia sẻ bài viết trên nhóm
     </h2>
     <div class="wrap-image">
-        <b-row>
-            <b-col>
-                <div class="card">
-                  <img class="card-img-top" src="/img/blog1.png" alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                    <a href="#" class="btn btn-primary button">XEM THÊM</a>
-                  </div>
-                </div>
-            </b-col>
-            <b-col>
-                <div class="card">
-                  <img class="card-img-top" src="/img/blog2.png" alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                    <a href="#" class="btn btn-primary button">XEM THÊM</a>
-                  </div>
-                </div>
-            </b-col>
-            <b-col>
-                <div class="card">
-                  <img class="card-img-top" src="/img/blog3.png" alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                    <a href="#" class="btn btn-primary button">XEM THÊM</a>
-                  </div>
-                </div>
-            </b-col>
-        </b-row>
-        <b-row class="row2">
-            <b-col>
-                <div class="card">
-                  <img class="card-img-top" src="/img/blog4.png" alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                    <a href="#" class="btn btn-primary button">XEM THÊM</a>
-                  </div>
-                </div>
-            </b-col>
-            <b-col>
-                <div class="card">
-                  <img class="card-img-top" src="/img/blog5.png" alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                    <a href="#" class="btn btn-primary button">XEM THÊM</a>
-                  </div>
-                </div>
-            </b-col>
-            <b-col>
-                <div class="card">
-                  <img class="card-img-top" src="/img/blog6.png" alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                    <a href="#" class="btn btn-primary button">XEM THÊM</a>
-                  </div>
-                </div>
-            </b-col>
-        </b-row>
+      <b-row>
+        <b-col>
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="/img/blog1.png"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content...
+              </p>
+              <a href="#" class="btn btn-primary button">XEM THÊM</a>
+            </div>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="/img/blog2.png"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content...
+              </p>
+              <a href="#" class="btn btn-primary button">XEM THÊM</a>
+            </div>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="/img/blog3.png"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content...
+              </p>
+              <a href="#" class="btn btn-primary button">XEM THÊM</a>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row class="row2">
+        <b-col>
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="/img/blog4.png"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content...
+              </p>
+              <a href="#" class="btn btn-primary button">XEM THÊM</a>
+            </div>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="/img/blog5.png"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content...
+              </p>
+              <a href="#" class="btn btn-primary button">XEM THÊM</a>
+            </div>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="/img/blog6.png"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content...
+              </p>
+              <a href="#" class="btn btn-primary button">XEM THÊM</a>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
     </div>
   </section>
 </template>
@@ -91,38 +133,38 @@
 }
 
 .wrap-image {
-    clear: both;
-    padding-top: 2rem;
-    .row2 {
-        margin-top: 2rem;
-    }
+  clear: both;
+  padding-top: 2rem;
+  .row2 {
+    margin-top: 2rem;
+  }
 }
 .card {
-    border: 0;
-    width: 90%;
-    .card-img-top {
-        border-radius: 0;
-        width: 100%;
-        height: 240px;
+  border: 0;
+  width: 90%;
+  .card-img-top {
+    border-radius: 0;
+    width: 100%;
+    height: 240px;
+  }
+  .card-body {
+    padding: 0;
+    .card-title {
+      width: 100%;
+      margin-top: -40px;
+      color: white;
+      height: 40px;
+      line-height: 40px;
+      padding-left: 10px;
+      background: #000000b3;
+      font-weight: 400;
     }
-    .card-body {
-        padding: 0;
-        .card-title {
-            width: 100%;
-            margin-top: -40px;
-            color: white;
-            height: 40px;
-            line-height: 40px;
-            padding-left: 10px;
-            background: #000000b3;
-            font-weight: 400;
-        }
-        .card-text {
-            letter-spacing: -0.4px;
-        }
-        .button {
-            border-radius: 0;
-        }
+    .card-text {
+      letter-spacing: -0.4px;
     }
+    .button {
+      border-radius: 0;
+    }
+  }
 }
 </style>
--- components/video/index.vue
+++ components/video/index.vue
@@ -1,14 +1,12 @@
 <template>
   <section class="wrap-video container">
-    <img
-      src="/img/cover.png"
-      class="image"
-    />
+    <span id="play"></span>
+    <img src="/img/video.png" class="image" />
     <video
-      src="https://www.youtube.com/watch?v=OWxS6EOOVkg"
+      src="/video/vietnam.mp4"
       preload="auto"
       loop="true"
-      class="video"
+      class="video hidden"
     ></video>
   </section>
 </template>
@@ -16,8 +14,20 @@
 <script>
 export default {
   mounted() {
-    const video1 = document.getElementsByClassName('video')[0]
-    video1.play()
+    const video = document.getElementsByClassName('video')[0]
+    const play = document.getElementById('play')
+    // set current time
+    video.currentTime = 50
+    play.addEventListener('click', () => {
+      video.classList.remove('hidden')
+      play.classList.add('hidden')
+      video.play()
+    })
+    video.addEventListener('click', () => {
+      video.classList.add('hidden')
+      play.classList.remove('hidden')
+      video.pause()
+    })
   }
 }
 </script>
@@ -28,13 +38,54 @@ export default {
   position: relative;
   .image {
     width: 100%;
-    height: 600px;
-    position: absolute;
+    height: 607px;
   }
-  .video-first {
+  .video {
     width: 100%;
-    height: 600px;
+    height: 625px;
+    margin-top: -607px;
+    animation: bounceIn 2s;
+  }
+}
+#play {
+  &:after {
+    animation: bounceIn 1s;
+    cursor: pointer;
+    pointer-events: auto;
+    content: '▶';
+    height: 5rem;
+    width: 5rem;
+    text-align: center;
+    left: 46.5%;
+    top: 40%;
     position: absolute;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2.5rem;
+    border-radius: 45px;
+    background-color: rgba(255, 255, 255, 0.637);
+    color: #a41c66;
+    text-shadow: 0 0.25rem 1rem rgba(black, 0.1);
+    padding-left: 6px;
+  }
+}
+.hidden {
+  display: none;
+  visibility: hidden;
+}
+@keyframes bounceIn {
+  0% {
+    transform: scale(0.1);
+    opacity: 0;
+  }
+  60% {
+    transform: scale(1.2);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(1);
   }
 }
 </style>
